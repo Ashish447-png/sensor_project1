0212c923c3f575745e91f3bb4a37b438a77d3dfc
Ashish Jana <ashishjana065@gmail.com>
2026-03-14 21:05:59 +0530

    modified folder

--- app.py
+++ app.py
@@ -1,48 +1,69 @@
-from flask import Flask, render_template, jsonify, request, send_file, redirect, url_for
+from flask import Flask, render_template, jsonify, request, send_file
 from src.exception import CustomException
 from src.logger import logging as lg
-import os, sys
+import os,sys
+
 
 from src.pipeline.train_pipeline import TrainingPipeline
 from src.pipeline.predict_pipeline import PredictionPipeline
 
-# Fixed: __name__
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
-    return redirect(url_for('upload'))
+    return "Welcome to my application"
+
+
+
 
 @app.route("/train")
 def train_route():
     try:
         train_pipeline = TrainingPipeline()
         train_pipeline.run_pipeline()
-        return "Training Completed. Go to /predict to upload file."
+
+
+        return "Training Completed."
+
+
     except Exception as e:
-        # Ideally, return a response here so the server doesn't just hang/crash
-        raise CustomException(e, sys)
+        raise CustomException(e,sys)
+
 
 @app.route('/predict', methods=['POST', 'GET'])
 def upload():
+   
     try:
+
+
+
+
         if request.method == 'POST':
+            # it is a object of prediction pipeline
             prediction_pipeline = PredictionPipeline(request)
+           
+            #now we are running this run pipeline method
             prediction_file_detail = prediction_pipeline.run_pipeline()
 
-            lg.info("Prediction completed. Downloading prediction file.")
 
-            return send_file(
-                prediction_file_detail.prediction_file_path,
-                download_name=prediction_file_detail.prediction_file_name,
-                as_attachment=True
-            )
+            lg.info("prediction completed. Downloading prediction file.")
+            return send_file(prediction_file_detail.prediction_file_path,
+                            download_name= prediction_file_detail.prediction_file_name,
+                            as_attachment= True)
+
+
+
+
         else:
             return render_template('upload_file.html')
     except Exception as e:
-        raise CustomException(e, sys)
+        raise CustomException(e,sys)
+   
 
-# Fixed: __name__ and __main__
+
+
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000)) 
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000, debug= True)
